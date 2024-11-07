54a7fcb13d18b9cb80f14ab1ecc474c6346ae1aa
sidigui16 <sidigui16@gmail.com>
2024-11-07 21:07:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// TYPED
+
+var typed = new Typed('.typed', {
+    strings: ["Je suis particulièrement intéressé par le Québec en raison de son écosystème dynamique en matière d'intelligence artificielle et de science des données. Mon diplôme en data analyst m'a donné une solide base en analyse prédictive, machine learning, visualisation de données.Je souhaite poursuivre mes études au Québec afin d'approfondir mes connaissances, de contribuer à la recherche, de devenir un acteur clé de l'innovation: Le Québec est un lieu privilégié pour innover et développer de nouvelles solutions basées sur les données. Je souhaite m'intégrer à cette communauté dynamique et contribuer à l'émergence de nouvelles technologies. À long terme, mon objectif est de devenir enseignant-chercheur afin de transmettre mes connaissances aux nouvelles générations et de contribuer à l'avancement de la recherche dans le domaine de la science des données."],
+    typeSpeed: 20,
+   
+  });
+
+
+  // AOS
+
+  Aos.init();
